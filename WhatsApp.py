59df673225af67e5038a075eb86ef3129ea969c0
Priyanshu Roy <52398646+priyanshu-9@users.noreply.github.com>
2021-10-05 22:49:11 +0530

    Create WhatsApp.py

---
+++ WhatsApp.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome("chromedriver")
+
+driver.get("https://web.whatsapp.com")
+
+def reps():
+    print("Do you want to send more msg to anyone")
+    askUser = input("Press y for Yes and n for No : ")
+    if (askUser == 'Y' or askUser == 'y'):
+        msg()
+    elif (askUser == 'N' or askUser == 'n'):
+        print("Thank you see you soon")
+    else:
+        print("Please Enter Valid option :\n")
+        reps()
+
+def msg():
+    name = input('\nEnter Group/User Name: ')
+    message = input("Enter your message to group/user: ")
+
+    # Find Whom to message     
+    try:
+        Count = int(input("Enter the message count:"))
+        
+        user = driver.find_element_by_xpath(
+        '//span[@title = "{}"]'.format(name))
+        user.click()
+    except:
+        msg()
+
+    text_box = driver.find_element_by_class_name("Fill_here")
+
+    #Send Button
+    for i in range(Count):
+        text_box.send_keys(message)
+        driver.find_element_by_class_name("Fill_here").click()
+    
+    reps()
+
+msg()
